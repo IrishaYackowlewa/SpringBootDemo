340cace809e8580343d6d61087bff1bcba36432c
IrishaYackowlewa <irisha.yackowlewa.homyak@gmail.com>
2020-09-07 13:21:26 +0300

    beginning of work

---
+++ src/main/java/com/exampleProject/demo/ProjectExampleApplication.java
@@ -0,0 +1,12 @@
+package com.exampleProject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectExampleApplication.class, args);
+	}
+}
